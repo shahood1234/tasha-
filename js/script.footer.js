b32bf8cdeeb55b6ef8ed151e617bb609f8a40942
Student <lab-201@student-metro.edu>
2022-06-14 12:38:39 +0500

    full

---
+++ js/script.footer.js
@@ -0,0 +1,57 @@
+document.querySelector("section.footer").innerHTML = `
+<div class="box-container row">
+
+
+
+            <div class="box col-3">
+            <a href="#" class="logo text-decoration-none"><i class="fas fa-utensils"></i>TASHA</a>
+            <p>TASHA- the home store is Branded Company famous for home kitchen appliances</p>
+            <a href="#">Expert Tips</a>
+            <a href="#">Register Complain</a>
+            <a href="#">Feedback</a>
+            <a href="#">Contact</a>
+            </div>
+            <div class="box col-2">
+                <h3>locations</h3>
+                <a href="#">pakistan</a>
+                <a href="#">japan</a>
+                <a href="#">russia</a>
+                <a href="#">USA</a>
+                <a href="#">france</a>
+            </div>
+
+            <div class="box col-2">
+                <h3>quick links</h3>
+                <a href="#">home</a>
+                <a href="#">dishes</a>
+                <a href="#">about</a>
+                <a href="#">menu</a>
+                <a href="#">reivew</a>
+                <a href="#">order</a>
+            </div>
+
+            <div class="box col-3">
+                <h3>contact info</h3>
+                <a href="#">+92 3132427808</a>
+                <a href="#">+81 90 1613 1747</a>
+                <a href="#">+7 (958) 576-01-99</a>
+                <a href="#">+1-844-284-7321</a>
+                <a href="#">+33 891 060 952</a>
+                <a href="#">shahoodmuhammad890@gmail.com</a>
+                <a href="#">mansoorkhan2745@gmail.com</a>
+                <a href="#">hasnainahmed567@gmail.com</a>
+            </div>
+
+            <div class="box col-2">
+                <h3>follow us</h3>
+                <a href="#">facebook</a>
+                <a href="#">twitter</a>
+                <a href="#">instagram</a>
+                <a href="#">linkedin</a>
+            </div>
+
+        </div>
+
+        <div class="credit"> copyright @ 2021 by <span>mr.Shahood,mr.Mansoor and mr.Hasnain</span> </div>
+
+`
